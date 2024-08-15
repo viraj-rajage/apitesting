e224c966eb94283aa38e4f947c9b616e72520c1c
viraj-rajage <virajrajage17@gmail.com>
2024-08-15 07:42:20 +0530

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Providers;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/test',function(Request $request){
+    return response()->json("hello api testing");
+});
